48b54a17a653fa4eb3d6f3a3c6cf13f166e39f9e
dheljohn <ejtubal10@gmail.com>
2026-08-04 10:58:17 +0800

    Added sentry

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+import { withSentryConfig } from '@sentry/nextjs';
+
 const nextConfig = {
-  output: "standalone",
+  output: 'standalone',
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: !process.env.CI,
+});
