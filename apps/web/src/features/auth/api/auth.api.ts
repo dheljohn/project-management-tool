ad27623e11e84e453fa54e94aea692d4b84cabb9
dheljohn <ejtubal10@gmail.com>
2026-08-18 17:59:47 +0800

    unfinish prob, already solve deployment on vercel and render as well as railway, getting 500 for calling login and register, cors maybe one of the reason, and the latest typeORM railway mysql DB migration

--- apps/web/src/features/auth/api/auth.api.ts
+++ apps/web/src/features/auth/api/auth.api.ts
@@ -1,4 +1,4 @@
-import api from "../../../../lib/api";
+import api from '../../../../lib/api';
 
 export interface LoginPayload {
   user_id: string;
@@ -11,10 +11,10 @@ export interface RegisterPayload {
 }
 
 export const loginUser = async (payload: LoginPayload) => {
-  const { data } = await api.post("/testlogin", payload);
+  const { data } = await api.post('/testlogin', payload);
   return data;
 };
 export const registerUser = async (payload: RegisterPayload) => {
-  const { data } = await api.post("/test01/create_member", payload);
+  const { data } = await api.post('/test01/create_member', payload);
   return data;
 };
