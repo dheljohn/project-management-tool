a9dd484600e62c529c8dd64ba6e2a6889ea79620
dheljohn <ejtubal10@gmail.com>
2026-08-18 13:18:17 +0800

    minor build try fix 3 configuring next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 import { withSentryConfig } from '@sentry/nextjs';
 
 const nextConfig = {
@@ -7,22 +8,11 @@ const nextConfig = {
 
 export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT, // should resolve to your Next.js project, not "node-nestjs"
-  outputFileTracingRoot: path.join(__dirname, '../../'),
+  project: process.env.SENTRY_PROJECT,
 
-  // Only print source map upload logs in CI
   silent: !process.env.CI,
 
-  // Upload a larger set of source maps for prettier stack traces
   widenClientFileUpload: true,
 
-  // Route browser requests through a Next.js rewrite to avoid ad blockers
-  // Make sure this path doesn't collide with your middleware matcher
   tunnelRoute: '/monitoring',
-
-  // Tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Automatic instrumentation of Vercel Cron Monitors (only relevant if deployed on Vercel)
-  automaticVercelMonitors: false,
 });
